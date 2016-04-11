a8ca79aebfaa2185298a72d065f4d60ea830238a
gareth clabon <glclabon@gmail.com>
2016-04-11 11:43:28 +0100

    Added Bulk & single message options

--- twilioFlaskBasic/twilioFlaskBasic.py
+++ twilioFlaskBasic/twilioFlaskBasic.py
@@ -4,11 +4,16 @@ from operator import itemgetter
 from flask import Flask, render_template, request
 #----------------------------------------------------------------------
 app = Flask(__name__)
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def home():
+    return render_template('./index.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def message():
+@app.route('/bulk', methods=['GET', 'POST'])
+def bulk():
     if request.method == 'GET':
-        return render_template('./form.html');
+        return render_template('./bulk.html');
     else:
         def formPost():
             def send():
@@ -16,10 +21,6 @@ def message():
                 with open("contacts.csv") as csvfile:
                     reader = csv.reader(csvfile)
                     msg=request.form['msg']
-                    #ensure message length short enough for single message
-                    while len(msg) > 145:
-                        print ("Error! Only 145 characters allowed!")
-                        msg = input("Enter the message to send: ")
                     for row in reader:
                         #remove header line from CSV file
                         if reader.line_num ==1:
@@ -37,10 +38,34 @@ def message():
                 twilioNumber = cfg["Twilio"]["twilioNumber"]
                 client = TwilioRestClient(sid, authToken)
                 message = client.messages.create(body=msgNamed, from_=twilioNumber, to=to,)
- 
+
             send()
         formPost()
         return render_template('success.html');
+
+@app.route('/single', methods=['GET','POST'])
+def single():
+    if request.method == 'GET':
+        return render_template('./single.html');
+    else:
+        def formPost():
+            def send():
+                msgNamed=request.form['msg']
+                to=request.form['to']
+                sendSms(msgNamed, to=to)
+
+            def sendSms(msgNamed, to):
+                """Get Twilio details from config file & send messages"""
+                cfg = configobj.ConfigObj("./config.ini")
+                sid = cfg["Twilio"]["sid"]
+                authToken = cfg["Twilio"]["authToken"]
+                twilioNumber = cfg["Twilio"]["twilioNumber"]
+                client = TwilioRestClient(sid, authToken)
+                message = client.messages.create(body=msgNamed, from_=twilioNumber, to=to,)
+            send()
+        formPost()
+        return render_template('success.html');
+
 
 @app.route('/reply', methods=['POST'])
 def reply():
